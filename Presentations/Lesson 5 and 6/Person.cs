955d8dbc31ff93400a29996693826500b5b8754f
Nicolai Birkmose Oksen <nicolaioksen@gmail.com>
2017-11-14 18:00:18 +0100

    Clean up directories and add assignment 2

--- Presentations/Lesson 5 and 6/Person.cs
+++ Presentations/Lesson 5 and 6/Person.cs
@@ -24,3 +24,22 @@ public enum Gender
 {
     Male, Female
 }
+
+
+public static void Main()
+{ 
+    string str = "5" + "\n" + 
+                 "5,5" + "\n" +
+                 "5555,0" + "\n" +
+                 "false";
+
+    using(TextReader tr = new StringReader(str))
+    {
+        int i = Int32.Parse(tr.ReadLine()); 
+        double d = Double.Parse(tr.ReadLine());  
+        decimal m = Decimal.Parse(tr.ReadLine());  
+        bool b = Boolean.Parse(tr.ReadLine());
+
+        Console.WriteLine("{0} \n{1} \n{2} \n{3}", i, d, m, b);
+    }
+}
